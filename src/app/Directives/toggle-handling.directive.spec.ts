79c653125735ba15f0b0089e5bb663adb7e88917
unknown <asadjavedtech@gmail.com>
2023-11-21 22:32:16 +0500

    Committing changes before Angular CLI updatnewfilees

---
+++ src/app/Directives/toggle-handling.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ToggleHandlingDirective } from './toggle-handling.directive';
+
+describe('ToggleHandlingDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ToggleHandlingDirective();
+    expect(directive).toBeTruthy();
+  });
+});
